9cc86d53ade2898231b34b21a39995e5da7820af
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-03-05 23:55:48 -0300

    feat: adicionando controller para atualizar o pet

--- src/controllers/pet.controller.ts
+++ src/controllers/pet.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { listPets, createPet } from "../services/pet.service";
+import { listPets, createPet, updatePet } from "../services/pet.service";
 
 export const create = async (req: Request, res: Response) => {
     const { name, type, description, deadlineVacination, vacinated } = req.body;
@@ -15,5 +15,15 @@ export const list = async (req: Request, res: Response) => {
     const cnpj = req.headers.cnpj as string;
     const pets = await listPets(cnpj);
     res.status(200).json(pets);
+    return;
+}
+
+export const update = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const { name, type, description, deadlineVacination } = req.body;
+    const petshopCnpj = req.headers.cnpj as string;
+    const deadlineDate = new Date(deadlineVacination); // Converter string para Date
+    const pet = await updatePet(id, name, type, description, deadlineDate, petshopCnpj);
+    res.status(200).json(pet);
     return;
 }

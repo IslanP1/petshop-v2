dd73fda10f1a86ac0f9c61b80d227131f4e6bc53
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-03-06 00:16:39 -0300

    feat: adicionando rota  para atualizar o pet

--- src/routes/pet.routes.ts
+++ src/routes/pet.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { list, create } from "../controllers/pet.controller";
+import { list, create, update } from "../controllers/pet.controller";
 import { checkExistsUserAccount } from "../middlewares/checkExistsUserAccount";
 
 const router = Router();
 
 router.post("/", checkExistsUserAccount, create);
 router.get("/", checkExistsUserAccount, list);
+router.put("/:id", checkExistsUserAccount, update);
+
 
 export default router;
